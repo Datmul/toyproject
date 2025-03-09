2682ed92396190d0555237af1c67f9df394b3dc4
Datmul <kimhaesoo1224@gmail.com>
2025-03-09 17:23:29 +0900

    테스트 웹페이지 띄우기 소스 커밋

---
+++ src/main/java/com/project/toyproject/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.project.toyproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	@GetMapping(value = {"/","/index"})
+	public String index() {
+		return "/index";
+	}
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "/hello";
+	}
+}
